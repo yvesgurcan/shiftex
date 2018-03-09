102fc788b9f50c60bf77d3eaa596647a0e42bf3f
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-03-08 22:19:19 -0800

    semi-colons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    ...state
+  }
+}
 
 class App extends Component {
-  render() {
+  render () {
+    console.log(this.props)
     return (
-      null
+      <div>
+        test
+      </div>
     )
   }
 }
 
-export default App;
+export default connect(mapStateToProps)(App)
---
+++ src/reducers.js
@@ -0,0 +1,122 @@
+import { combineReducers } from 'redux'
+import moment from 'moment'
+
+function timetracking (state = {}, action) {
+
+  let newState = {...state}
+
+  switch (action.type) {
+    default:
+      break
+    
+    case 'STORE_SHIFT_DAY': {
+      newState = {
+        ...state,
+        day: action.day,
+      }
+      break
+    }
+
+    case 'STORE_SHIFTS': {
+      newState = {
+        ...state,
+        shifts: [...action.shifts],
+        dailyTotals: [...action.dailyTotals],
+      }
+
+      break
+    }
+
+    case 'ADD_SHIFT': {
+      const day = moment(state.day)
+      const updatedShifts = [...state.shifts, {
+        shiftId: 'newShift',
+        day: day.format('YYYY-MM-DD'),
+        start: action.start,
+        end: action.end,
+      }]
+      newState = {
+        ...state,
+        shifts: updatedShifts,
+      }
+      break
+    }
+
+    case 'STORE_NEW_SHIFT_ID': {
+      let updatedShifts = state.shifts.map(shift => {
+        let updatedShift = {...shift}
+        if (updatedShift.shiftId === action.tempShiftId) {
+          updatedShift.shiftId = action.newShiftId
+        }
+
+        return updatedShift
+      })
+
+      newState = {
+        ...state,
+        shifts: updatedShifts,
+      }
+
+      break
+    }
+
+    case 'UPDATE_SHIFT': {
+      let shifts = [...state.shifts]
+      if (action.name && action.value) {
+        shifts = [...state.shifts].map(shift => {
+          let augmentedShift = {...shift}
+          if (shift.shiftId === action.shiftId) {
+            augmentedShift = {
+              ...augmentedShift,
+              [action.name]: action.value,
+            }
+          }
+          
+          return augmentedShift
+        })  
+      }
+
+      const dailyTotals = [...state.dailyTotals].map(dailyTotal => {
+        let recalculatedDailyTotal = {...dailyTotal}
+        if (dailyTotal.day === state.day) {
+          recalculatedDailyTotal = {
+            ...recalculatedDailyTotal,
+            total: [...shifts].map(shift => {
+              if (!shift.end) {
+                return moment.duration(moment().diff(moment(shift.start))).asMinutes()
+              }
+              return moment.duration(moment(shift.end).diff(moment(shift.start))).asMinutes()
+            }).reduce((sum, value) => sum + value),
+          }
+        }
+        return recalculatedDailyTotal
+      })
+
+      newState = {
+        ...state,
+        shifts,
+        dailyTotals,
+      }
+
+      break
+    }
+
+    case 'DELETE_SHIFT': {
+      const shifts = [...state.shifts].filter(shift => shift.shiftId !== action.shiftId)
+      newState = {
+        ...state,
+        shifts,
+      }
+
+      break
+    }
+
+  }
+
+
+  return newState
+}
+
+export default combineReducers({
+  timetracking,
+})
