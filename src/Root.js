ff0b2434566a5c2839561dcaaf213ab2301a9c36
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-03-08 22:18:39 -0800

    root

---
+++ src/Root.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
+import App from './App'
+
+const store = createStore(
+  reducers,
+  {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+export default () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path='/' component={App} />
+        <Route path='/:year(2[0-9][0-9]{2})/:month([1-9]|0[1-9]|1[0-2])/:day([1-9]|0[1-9]|[1-2][0-9]|3[0-1])' component={App} />
+      </Switch>
+    </Router>
+  </Provider>
+)
