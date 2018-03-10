0cecfd14275d9affdd48372464c587484b4566f9
Yves Gurcan <gurcan.yves+github@gmail.com>
2018-03-09 23:23:16 -0800

    stylesheet

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import Root from './Root'
+import './style.css'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
